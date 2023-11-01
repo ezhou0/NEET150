6b384a418f75996f733b36a1f338fd1c4e354128
Edwin <edwinzhou99@gmail.com>
2023-11-01 11:39:34 -0700

    add 242

--- array.js
+++ array.js
@@ -13,3 +13,10 @@ var containsDuplicate = function(nums) {
 
 //***242. VALID ANAGRAM***//
 //Given two strings s and t, return true if t is an anagram of s, and false otherwise. An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.//
+
+var isAnagram = function(s, t){
+    let str1 = s.split("").sort().join("");
+    let str2 = t.split("").sort(). join("");
+
+    return str1 === str2;
+};
